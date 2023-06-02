124f2a43d16f89f26bfb68388f383abb580b8c47
davidamurim7 <davidamurim7@gmail.com>
2023-06-02 08:37:29 -0300

    add dialog

--- src/app/pages/create/create.component.ts
+++ src/app/pages/create/create.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import * as moment from 'moment';
-import { ProductElementService } from '../../core/services/productElement.service';
-import { ActivatedRoute, Router } from '@angular/router';
-import { ProductElement } from '../../shared/interfaces/ProductElement';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import * as moment from "moment";
+import { ProductElementService } from "../../core/services/productElement.service";
+import { ActivatedRoute, Router } from "@angular/router";
+import { ProductElement } from "../../shared/interfaces/ProductElement";
 
 @Component({
-  selector: 'app-create',
-  templateUrl: './create.component.html',
-  styleUrls: ['./create.component.scss'],
+  selector: "app-create",
+  templateUrl: "./create.component.html",
+  styleUrls: ["./create.component.scss"],
   providers: [ProductElementService],
 })
 export class CreateComponent implements OnInit {
@@ -22,25 +22,39 @@ export class CreateComponent implements OnInit {
     private activatedRoute: ActivatedRoute
   ) {
     this.data = fb.group({
-      name: ['', Validators.required],
-      cost: ['', Validators.required],
-      unit: ['', Validators.required],
-      validity: ['', Validators.required],
-      perishable: ['', Validators.required],
-      quantity: ['', Validators.required],
-      fabrication: ['', Validators.required],
+      name: [
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.maxLength(50),
+          Validators.pattern("^[a-zA-Z ]*$"),
+        ]),
+      ],
+      cost: ["", Validators.required],
+      unit: ["", Validators.required],
+      validity: ["", Validators.required],
+      perishable: ["", Validators.required],
+      quantity: ["", Validators.required],
+      fabrication: ["", Validators.required],
     });
   }
 
   ngOnInit() {
-    let id = this.activatedRoute.snapshot.queryParamMap.get('id');
+    let id = this.activatedRoute.snapshot.queryParamMap.get("id");
     if (id) {
       this.productElementService
         .showElement(id)
         .subscribe((response: ProductElement) => {
           this.idProduct = response.id;
           this.data = this.fb.group({
-            name: [response.name, Validators.required],
+            name: [
+              response.name,
+              Validators.compose([
+                Validators.required,
+                Validators.maxLength(50),
+                Validators.pattern("^[a-zA-Z ]*$"),
+              ]),
+            ],
             cost: [response.cost, Validators.required],
             unit: [response.unit, Validators.required],
             validity: [response.validity, Validators.required],
@@ -56,20 +70,20 @@ export class CreateComponent implements OnInit {
     let newValidity: moment.Moment = moment
       .utc(this.data.value.validity)
       .local();
-    this.data.value.validity = newValidity.format('YYYY-MM-DD');
+    this.data.value.validity = newValidity.format("YYYY-MM-DD");
 
     let newFabrication: moment.Moment = moment
       .utc(this.data.value.fabrication)
       .local();
-    this.data.value.fabrication = newFabrication.format('YYYY-MM-DD');
+    this.data.value.fabrication = newFabrication.format("YYYY-MM-DD");
 
     if (!this.idProduct) {
       this.productElementService.createElements(this.data.value).subscribe(
         () => {
-          this.router.navigate(['/']);
+          this.router.navigate(["/"]);
         },
         (err) => {
-          console.log('Error occurred', err);
+          console.log("Error occurred", err);
         }
       );
     } else {
@@ -77,10 +91,10 @@ export class CreateComponent implements OnInit {
         .editElement(this.idProduct, this.data.value)
         .subscribe(
           () => {
-            this.router.navigate(['/']);
+            this.router.navigate(["/"]);
           },
           (err) => {
-            console.log('Error occurred', err);
+            console.log("Error occurred", err);
           }
         );
     }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,31 +1,38 @@
-import { Component, Input, ViewChild } from '@angular/core';
-import { ProductElement } from 'src/app/shared/interfaces/ProductElement';
-import { ProductElementService } from '../../core/services/productElement.service';
-import { Router } from '@angular/router';
+import { Component, Input, ViewChild } from "@angular/core";
+import { ProductElement } from "src/app/shared/interfaces/ProductElement";
+import { ProductElementService } from "../../core/services/productElement.service";
+import { Router } from "@angular/router";
+import { DialogModule } from "../../shared/dialog/dialog.module";
+import { MatDialog } from "@angular/material/dialog";
+import {
+  ConfirmDialogModel,
+  DialogComponent,
+} from "../../shared/dialog/dialog-component/dialog.component";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
   providers: [ProductElementService],
 })
 export class HomeComponent {
   displayedColumns: string[] = [
-    'name',
-    'unit',
-    'quantity',
-    'cost',
-    'perishable',
-    'validity',
-    'fabrication',
-    'actions',
+    "name",
+    "unit",
+    "quantity",
+    "cost",
+    "perishable",
+    "validity",
+    "fabrication",
+    "actions",
   ];
   dataSource!: ProductElement[];
   @Input() currentDate = new Date().toISOString();
 
   constructor(
     public productElementService: ProductElementService,
-    private router: Router
+    private router: Router,
+    public dialog: MatDialog
   ) {
     this.refreshTable();
   }
@@ -39,17 +46,29 @@ export class HomeComponent {
   }
 
   edit(id: number): void {
-    this.router.navigate(['/create'], { queryParams: { id: id } });
+    this.router.navigate(["/create"], { queryParams: { id: id } });
   }
 
   delete(id: number): void {
-    this.productElementService.deleteElement(id).subscribe(
-      () => {
-        this.refreshTable();
-      },
-      (err) => {
-        console.log('Error occurred', err);
+    const message = `Tem certeza que deseja excluir este produto?`;
+    const dialogData = new ConfirmDialogModel("Confirmar exclusão", message);
+
+    const dialogRef = this.dialog.open(DialogComponent, {
+      maxWidth: "500px",
+      data: dialogData,
+    });
+
+    dialogRef.afterClosed().subscribe((dialogResult) => {
+      if (dialogResult) {
+        this.productElementService.deleteElement(id).subscribe(
+          () => {
+            this.refreshTable();
+          },
+          (err) => {
+            console.log("Error occurred", err);
+          }
+        );
       }
-    );
+    });
   }
 }
